7652543cca6df0c9b993ba2e8010ae5f459414e1
Aamina Bibi <aaminabibi2324@gmail.com>
2024-08-11 19:04:37 +0500

    Pressable Update

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import ModalBox from './Concepts/ModalBox';
+import Pressbale from './Concepts/Pressable';
 
-AppRegistry.registerComponent(appName, () => ModalBox);
+
+
+//AppRegistry.registerComponent(appName, () => App);
+//AppRegistry.registerComponent(appName, () => ModalBox);
+AppRegistry.registerComponent(appName, () => Pressbale);
