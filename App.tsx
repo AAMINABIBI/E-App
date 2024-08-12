0f5dfb3d76f3db0c6b8dfffa6d5429b5725db945
Aamina Bibi <aaminabibi2324@gmail.com>
2024-08-12 15:41:24 +0500

    stack navigation update

--- App.tsx
+++ App.tsx
@@ -9,12 +9,17 @@ import ModalBox from './Concepts/ModalBox';
 import Pressbale from './Concepts/Pressable';
 import BarStatus from './Concepts/BarStatus';
 import CustomModal from './Concepts/CustomModal'
-
-const App = () => {
-  return (
-  <CustomModal/>
-
-  )
-}
+import StackNavigation from './Concepts/StackNavigation';
+const App =
+  () => {
+    return (
+      // <Pressbale/>
+      //<ModalBox/>
+      // <Pressbale/>
+      //<BarStatus/>
+      //<CustomModal/>
+      <StackNavigation />
+    )
+  }
 
 export default App
