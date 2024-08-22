90480abe6a3296aedb87b9999aaefb7457260925
Aamina Bibi <aaminabibi2324@gmail.com>
2024-08-22 20:46:52 +0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,25 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 import React from 'react'
-import ModalBox from './Concepts/ModalBox';
-import Pressbale from './Concepts/Pressable';
-import BarStatus from './Concepts/BarStatus';
-import CustomModal from './Concepts/CustomModal'
-import StackNavigation from './Concepts/StackNavigation';
-const App =
-  () => {
-    return (
-      // <Pressbale/>
-      //<ModalBox/>
-      // <Pressbale/>
-      //<BarStatus/>
-      //<CustomModal/>
-      <StackNavigation />
-    )
-  }
+import { Text, View } from 'react-native'
+import StackNavigator from './navigation/StackNavigator'
+
+const App = () => {
+  return (
+<StackNavigator/>
+  )
+}
 
 export default App
--- index.js
+++ index.js
@@ -5,16 +5,11 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import ModalBox from './Concepts/ModalBox';
-import Pressbale from './Concepts/Pressable';
-import BarStatus from './Concepts/BarStatus';
-import CustomModal from './Concepts/CustomModal'
+
 
 
 AppRegistry.registerComponent(appName, () => App);
-//terComponent(appName, () => Pressbale);
-//AppRegistry.registerComponent(appName, () => BarStatus);
-//AppRegistry.registerComponent(appName, () => CustomModal);
+
 
 
 
